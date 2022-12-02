9dd02af5653bbd202bda9a66fb2efa391ef118f7
Jay Soffian <jaysoffian@gmail.com>
2022-12-02 11:55:23 -0500

    Day 2 refactored

--- day02/solve.py
+++ day02/solve.py
@@ -10,32 +10,31 @@ HERE = Path(__file__).parent
 INPUT = (HERE / "input.txt").read_text().strip()
 # INPUT = (HERE / "sample.txt").read_text().strip()
 
+ROCK, PAPER, SCISSORS = "Rock", "Paper", "Scissors"
+WIN, LOSE, DRAW = "Win", "Lose", "Draw"
+SCORES = {ROCK: 1, PAPER: 2, SCISSORS: 3, LOSE: 0, DRAW: 3, WIN: 6}
+C1 = {"A": ROCK, "B": PAPER, "C": SCISSORS}
+
 
 def part1():
-    ROCK, PAPER, SCISSORS = "Rock", "Paper", "Scissors"
-    WIN, LOSE, DRAW = "Win", "Lose", "Draw"
-    SCORES = {ROCK: 1, PAPER: 2, SCISSORS: 3, LOSE:0, DRAW:3, WIN:6}
-    C1 = {"A": ROCK, "B": PAPER, "C": SCISSORS}
     C2 = {"X": ROCK, "Y": PAPER, "Z": SCISSORS}
+    OUTCOMES = {
+        # s1, s2: s2 outcome
+        (ROCK, ROCK): DRAW,
+        (ROCK, PAPER): WIN,
+        (ROCK, SCISSORS): LOSE,
+        (PAPER, ROCK): LOSE,
+        (PAPER, PAPER): DRAW,
+        (PAPER, SCISSORS): WIN,
+        (SCISSORS, ROCK): WIN,
+        (SCISSORS, PAPER): LOSE,
+        (SCISSORS, SCISSORS): DRAW,
+    }
     total = 0
     for line in INPUT.splitlines():
         c1, c2 = line.split()
-        s1 = C1[c1]
-        s2 = C2[c2]
-        if s1 == s2:
-            outcome = DRAW
-        elif (s1, s2) == (ROCK, SCISSORS):
-            outcome = LOSE
-        elif (s1, s2) == (ROCK, PAPER):
-            outcome = WIN
-        elif (s1, s2) == (PAPER, ROCK):
-            outcome = LOSE
-        elif (s1, s2) == (PAPER, SCISSORS):
-            outcome = WIN
-        elif (s1, s2) == (SCISSORS, ROCK):
-            outcome = WIN
-        elif (s1, s2) == (SCISSORS, PAPER):
-            outcome = LOSE
+        s1, s2 = C1[c1], C2[c2]
+        outcome = OUTCOMES[(s1, s2)]
         score = SCORES[s2] + SCORES[outcome]
         total += score
         # print(score)
@@ -43,31 +42,24 @@ def part1():
 
 
 def part2():
-    ROCK, PAPER, SCISSORS = "Rock", "Paper", "Scissors"
-    WIN, LOSE, DRAW = "Win", "Lose", "Draw"
-    SCORES = {ROCK: 1, PAPER: 2, SCISSORS: 3, LOSE:0, DRAW:3, WIN:6}
-    C1 = {"A": ROCK, "B": PAPER, "C": SCISSORS}
     C2 = {"X": LOSE, "Y": DRAW, "Z": WIN}
+    SHAPES = {
+        # s1, s2 outcome: s2
+        (ROCK, DRAW): ROCK,
+        (ROCK, WIN): PAPER,
+        (ROCK, LOSE): SCISSORS,
+        (PAPER, LOSE): ROCK,
+        (PAPER, DRAW): PAPER,
+        (PAPER, WIN): SCISSORS,
+        (SCISSORS, WIN): ROCK,
+        (SCISSORS, LOSE): PAPER,
+        (SCISSORS, DRAW): SCISSORS,
+    }
     total = 0
     for line in INPUT.splitlines():
         c1, c2 = line.split()
-        s1 = C1[c1]
-        outcome = C2[c2]
-        if outcome == WIN:
-            s2 = {
-                ROCK: PAPER,
-                PAPER: SCISSORS,
-                SCISSORS: ROCK,
-            }[s1]
-        elif outcome == LOSE:
-            s2 = {
-                ROCK: SCISSORS,
-                PAPER: ROCK,
-                SCISSORS: PAPER,
-            }[s1]
-        else:
-            assert outcome == DRAW
-            s2 = s1
+        s1, outcome = C1[c1], C2[c2]
+        s2 = SHAPES[(s1, outcome)]
         score = SCORES[s2] + SCORES[outcome]
         total += score
         # print(f"{outcome} to {s1} = {s2} ({score})")
