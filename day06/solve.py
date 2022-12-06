1f59ad3884f90ce39a44418ab065dc74bb735151
Jay Soffian <jaysoffian@gmail.com>
2022-12-06 11:46:50 -0500

    Day 6: move assertions into test()
    Allows for testing w/`py.test solve.py`.

--- day06/solve.py
+++ day06/solve.py
@@ -20,7 +20,7 @@ part1 = partial(detect, length=4)
 part2 = partial(detect, length=14)
 
 
-def main():
+def test():
     assert part1("abcd") == 4
     assert part1("aabbccddeeff") == 0
     assert part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
@@ -28,13 +28,16 @@ def main():
     assert part1("nppdvjthqldpwncqszvftbrmjlhg") == 6
     assert part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
     assert part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
-    print(part1(INPUT))
-
     assert part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19
     assert part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23
     assert part2("nppdvjthqldpwncqszvftbrmjlhg") == 23
     assert part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29
     assert part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26
+
+
+def main():
+    test()
+    print(part1(INPUT))
     print(part2(INPUT))
 
 
