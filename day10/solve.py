6927429f7d3791d43357152bf98431095bc6939f
Jay Soffian <jaysoffian@gmail.com>
2022-12-10 14:50:46 -0500

    Day 10: refactor

--- day10/solve.py
+++ day10/solve.py
@@ -10,39 +10,34 @@ INPUT = (HERE / "input.txt").read_text().strip().splitlines()
 
 
 class Cpu:
-    INSTRUCTIONS = {
-        "noop": 1,
-        "addx": 2,
-    }
-
-    def __init__(self):
-        self.cycle = 1
+    def __init__(self, lines):
+        self.lines = lines
         self.x = 1
 
-    def run(self, lines):
-        for line in lines:
+    def __iter__(self):
+        for i, line in enumerate(self.lines, 1):
             args = line.split()
-            inst = args.pop(0)
-            for _ in range(Cpu.INSTRUCTIONS[inst]):
-                yield
-                self.cycle += 1
-            if inst == "addx":
-                self.x += int(args[0])
-
-    def __str__(self):
-        return f"{self.cycle} {self.x}"
+            inst = args[0]
+            if inst == "noop":
+                assert len(args) == 1, f"{i}: {line!r}"
+                yield self.x
+            elif inst == "addx":
+                assert len(args) == 2, f"{i}: {line!r}"
+                yield self.x
+                yield self.x
+                self.x += int(args[1])
+            else:
+                assert False, f"{i}: {line!r}"
+        yield self.x
 
 
 class Crt:
-    def __init__(self):
-        self.cycle = 0
-        self.rows = [["."] * 40 for _ in range(6)]
-
-    def tick(self, x):
-        row, col = divmod(self.cycle, 40)
-        lit = col - 1 <= x <= col + 1
-        self.rows[row % 6][col] = "#" if lit else "."
-        self.cycle += 1
+    def __init__(self, cpu, num_rows=6, num_cols=40):
+        self.rows = [["."] * num_cols for _ in range(num_rows)]
+        for i, x in zip(range(0, num_rows * num_cols), cpu):
+            row, col = divmod(i, num_cols)
+            lit = col - 1 <= x <= col + 1
+            self.rows[row % num_rows][col] = "#" if lit else "."
 
     def __str__(self):
         return "\n".join("".join(row) for row in self.rows)
@@ -50,54 +45,31 @@ class Crt:
 
 def part1(lines):
     total = 0
-    cpu = Cpu()
-    for _ in cpu.run(lines):
-        if cpu.cycle in (20, 60, 100, 140, 180, 220):
-            total += cpu.x * cpu.cycle
+    for i, x in enumerate(Cpu(lines), 1):
+        if i in range(20, 221, 40):
+            total += i * x
     return total
 
 
 def part2(lines):
-    cpu = Cpu()
-    crt = Crt()
-    for _ in cpu.run(lines):
-        crt.tick(cpu.x)
-    return str(crt)
+    return str(Crt(Cpu(lines)))
 
 
 def test1a():
-    cpu = Cpu()
-    for i, _ in enumerate(cpu.run(["noop", "addx 3", "addx -5"]), 1):
-        # print(cpu)
-        assert i == cpu.cycle
-        assert (
-            cpu.x
-            == {
-                1: 1,
-                2: 1,
-                3: 1,
-                4: 4,
-                5: 4,
-                6: -1,
-            }[i]
-        )
+    ops = ["noop", "addx 3", "addx -5"]
+    expect = [1, 1, 1, 4, 4, -1]
+    for x, y in zip(expect, Cpu(ops)):
+        assert x == y
 
 
 def test1b():
-    cpu = Cpu()
     total = 0
-    for _ in cpu.run(SAMPLE):
-        x = {
-            20: 21,
-            60: 19,
-            100: 18,
-            140: 21,
-            180: 16,
-            220: 18,
-        }.get(cpu.cycle)
-        assert x is None or cpu.x == x, f"{x} {cpu.x}"
-        if x:
-            total += cpu.x * cpu.cycle
+    expect = dict(zip(range(20, 221, 40), [21, 19, 18, 21, 16, 18]))
+    for i, x in enumerate(Cpu(SAMPLE), 1):
+        if i in expect:
+            assert x == expect[i], f"i={i}"
+            total += i * x
+    assert i == 241
     assert total == 13140, total
     assert part1(SAMPLE) == 13140, part1(SAMPLE)
 
@@ -118,11 +90,10 @@ def test2():
 def main():
     test1a()
     test1b()
-    test2()
-
     print(part1(INPUT))
     assert part1(INPUT) == 16880
 
+    test2()
     print(part2(INPUT))
     expect = """
     ###..#..#..##..####..##....##.###..###..
